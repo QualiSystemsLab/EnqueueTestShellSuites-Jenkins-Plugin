c90cf126272ebd22d8f12e928a72f41fbc04ebbf
AharonShachar <aharon.s@qualisystems.com>
2016-12-21 17:00:59 +0200

    Support work with topologies

---
+++ src/main/java/org/jenkinsci/plugins/cloudshell/Strcture/RequirementsInput.java
@@ -0,0 +1,13 @@
+package org.jenkinsci.plugins.cloudshell.Strcture;
+
+import java.util.List;
+
+/**
+ * Created by aharon.s on 12/21/2016.
+ */
+public class GlobalInput
+{
+    public String Name;
+    public String Value;
+    public List<String> PossibleValues;
+}
